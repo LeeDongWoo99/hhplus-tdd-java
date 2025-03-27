8303e79f5749e42a1f6b7524b38565c059ffcea3
dongwooLee <dleh684@gmail.com>
2025-03-27 14:06:02 +0900

    first commit

---
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceImplTest.java
@@ -0,0 +1,24 @@
+package io.hhplus.tdd.point.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointServiceImplTest {
+
+    @Test
+    void getUserPoint() {
+    }
+
+    @Test
+    void getPointHistories() {
+    }
+
+    @Test
+    void chargePoint() {
+    }
+
+    @Test
+    void usePoint() {
+    }
+}
